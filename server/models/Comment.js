b17b377f4ab787f268a3ca5c46fba6daffc97384
Ankan Guchait <ankanguchait239@gmail.com>
2026-09-05 13:26:40 +0530

    feat(auth): add roles and protected account APIs

--- server/models/Comment.js
+++ server/models/Comment.js
@@ -1,12 +1,17 @@
 import mongoose from "mongoose";
 
-const commentSchema = new mongoose.Schema({
-    blog : {type:mongoose.Schema.Types.ObjectId, ref: 'blog', required: true},
-    name: {type: String, required: true},
-    content: {type: String, required: true},
-    isApproved: {type: Boolean, default: false}
-},{timestamps: true});
+const commentSchema = new mongoose.Schema(
+  {
+    blog: { type: mongoose.Schema.Types.ObjectId, ref: "blog", required: true },
+    // Optional to preserve comments created before reader accounts existed.
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "user" },
+    name: { type: String, required: true },
+    content: { type: String, required: true },
+    isApproved: { type: Boolean, default: false },
+  },
+  { timestamps: true },
+);
 
-const Comment = mongoose.model('comment', commentSchema);
+const Comment = mongoose.model("comment", commentSchema);
 
 export default Comment;
